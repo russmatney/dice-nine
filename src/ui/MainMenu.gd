4f874982a3eebd5abe84e24ad6850d6016447de1
Russell Matney <russell.matney@gmail.com>
2022-07-16 17:23:14 -0400

    feat: lots of wrestling, but now a title screen

---
+++ src/ui/MainMenu.gd
@@ -0,0 +1,13 @@
+extends Control
+
+func _on_Credits_pressed():
+  # TODO credits
+	pass # Replace with function body.
+
+
+func _on_Controls_pressed():
+  # TODO controls
+	pass # Replace with function body.
+
+func _on_StartGame_pressed():
+	Nav.goto_level_1()
